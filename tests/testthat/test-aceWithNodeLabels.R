245e7a89d6d62d4377be226943d2ffcf6fe66199
Emmanuel Paradis <emmanuelparadis@users.noreply.github.com>
2024-01-28 09:50:42 +0700

    Create test-aceWithNodeLabels.R
    add tests from PR #106 in ape

---
+++ tests/testthat/test-aceWithNodeLabels.R
@@ -0,0 +1,15 @@
+test_that("adding node labels to ace output works", {
+    ## Example with no node labels
+    data(bird.orders)
+    x <- rnorm(23)
+    ### Compare the three methods for continuous characters:
+    out <- ace(x, bird.orders)
+    expect_equal(names(out$ace), as.character(seq(from = 1+Ntip(bird.orders), to = Nnode(bird.orders)+Ntip(bird.orders))))
+    expect_equal(rownames(out$CI95), as.character(seq(from = 1+Ntip(bird.orders), to = Nnode(bird.orders)+Ntip(bird.orders))))
+
+    ## Adding node labels
+    phy_nodes <- makeNodeLabel(bird.orders)
+    out <- ace(x, phy_nodes)
+    expect_equal(names(out$ace), phy_nodes$node.label)
+    expect_equal(rownames(out$CI95), phy_nodes$node.label)
+})
